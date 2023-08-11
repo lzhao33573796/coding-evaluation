aca18f65dcc6512a43d78667edff0a5ecbc19b25
lzhao33573796 <56361621+lzhao33573796@users.noreply.github.com>
2023-08-10 22:52:33 -0700

    Add files via upload

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,10 +1,12 @@
 package com.aa.act.interview.org;
 
-import java.util.Optional;
+import java.util.*;
 
 public abstract class Organization {
 
     private Position root;
+
+    private static int id = 0;
     
     public Organization() {
         root = createOrganization();
@@ -20,7 +22,38 @@ public abstract class Organization {
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
+        // check if there is such title for all current positions
+        Optional<Position> found = getTargetPosition(root, title);
+        if (found.isPresent()) {
+            found.get().setEmployee(Optional.of(new Employee(id++, person)));
+            return found;
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * recursively find the position with the specific title
+     *
+     * @param cur current position
+     * @param title target position title
+     * @return Optional<Position>
+     */
+    private Optional<Position> getTargetPosition(Position cur, String title) {
+        // if this position is the one we want, return it
+        if (cur.getTitle().equals(title)) {
+            return Optional.of(cur);
+        }
+        // if this position has no direct report
+        // return empty
+        if (cur.getDirectReports().isEmpty()) {
+            return Optional.empty();
+        }
+        for (Position p : cur.getDirectReports()) {
+            Optional<Position> found = getTargetPosition(p, title);
+            if (found.isPresent()) {
+                return found;
+            }
+        }
         return Optional.empty();
     }
 
